a930f70c921e6c1d4a00b3528813584d864e5c20
Alexander <alex.drolshagen@gmail.com>
2019-04-11 20:54:32 -0600

    Updates

---
+++ precache-manifest.cb178c855757723a0a6b803e5d556b01.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "71c944fec9b235882788",
+    "url": "/19-clicky-game/static/js/runtime~main.2cac84bf.js"
+  },
+  {
+    "revision": "ad48c52c61d0bdc8638c",
+    "url": "/19-clicky-game/static/js/main.519aca2b.chunk.js"
+  },
+  {
+    "revision": "a49af3237dbddbb747fb",
+    "url": "/19-clicky-game/static/js/2.650383ae.chunk.js"
+  },
+  {
+    "revision": "ad48c52c61d0bdc8638c",
+    "url": "/19-clicky-game/static/css/main.0824daf8.chunk.css"
+  },
+  {
+    "revision": "a5c1ef8d0d0bd2701bec713c6cc6212c",
+    "url": "/19-clicky-game/index.html"
+  }
+];
